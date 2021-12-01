1f4df320f94d8fd9ac8e17abf1ab63f34998ca7a
SangNguyenchi <75151386+SangNguyenchi@users.noreply.github.com>
2021-12-01 10:48:36 +0700

    Update js.js

--- js.js
+++ js.js
@@ -1,25 +1,6 @@
 var $ = function (id) {
   return document.getElementById(id);
 };
-// Load the AWS SDK for Node.js
-var AWS = require('aws-sdk');
-
-var bucketName = 'sangnc1-aws-test';
-var bucketRegion = 'us-west-2';
-var IdentityPoolId = IDENTITYPOOLID;
-
-AWS.config.update({
-                region: bucketRegion,
-                credentials: new AWS.CognitoIdentityCredentials({
-                IdentityPoolId: IdentityPoolId
-                })
-            });
-
-            var s3 = new AWS.S3({
-                apiVersion: '2006-03-01',
-                params: {Bucket: bucketName}
-        });
-
 const uploadFile = (e) => {
   $("result").innerText = e.target.value;
 
@@ -28,28 +9,7 @@ const uploadFile = (e) => {
 
 const uploadFileToS3 = (e) => {
   // handle send to S3 
-   var files = document.getElementById('fileUpload').files;
-   if (files) 
-   {
-     var file = files[0];
-     var fileName = file.name;
-     var filePath = 'my-first-bucket-path/' + fileName;
-     var fileUrl = 'https://' + bucketRegion + '.amazonaws.com/my-    first-bucket/' +  filePath;
-     s3.upload({
-        Key: filePath,
-        Body: file,
-        ACL: 'public-read'
-        }, function(err, data) {
-        if(err) {
-        reject('error');
-        }
-        alert('Successfully Uploaded!');
-        }).on('httpUploadProgress', function (progress) {
-        var uploaded = parseInt((progress.loaded * 100) / progress.total);
-        $("progress").attr('value', uploaded);
-      });
-   }
-};
+   
 window.onload = function () {
   $("upload").onchange = uploadFile;
   //$("uploadS3").onchange = uploadFileToS3;
