b7a505c83c127f09c9fd5e2140d0f97210c6eb3b
SangNguyenchi <75151386+SangNguyenchi@users.noreply.github.com>
2021-12-01 09:45:41 +0700

    Update js.js

--- js.js
+++ js.js
@@ -6,7 +6,7 @@ var AWS = require('aws-sdk');
 
 var bucketName = 'sangnc1-aws-test';
 var bucketRegion = 'us-west-2';
-var IdentityPoolId = arn:aws:iam::944514424264:role/Ec2AccessToS3;
+var IdentityPoolId = IDENTITYPOOLID;
 
 AWS.config.update({
                 region: bucketRegion,
